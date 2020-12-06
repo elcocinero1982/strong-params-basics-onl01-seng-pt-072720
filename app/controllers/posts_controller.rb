e80819383988a1464eb40d367a3b92ec17478cad
jose luis <cortezluisjose@gmail.com>
2020-12-06 04:02:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,7 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-  @post.update(post_params)
-  redirect_to post_path(@post)
+	  
 end
  
 private
