ca28bcb62fb999c7c3e033441a502833827e35e8
jose luis <cortezluisjose@gmail.com>
2020-12-06 03:59:38 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,15 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  
+	  @post = Post.find(params[:id])
+  @post.update(post_params)
+  redirect_to post_path(@post)
+end
+ 
+private
+ 
+def post_params
+  params.require(:post).permit(:title, :description)
 	end
 
 	def edit
