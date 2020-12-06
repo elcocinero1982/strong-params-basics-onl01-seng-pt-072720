e62859ef05d39f4f3c52ce067b4c8bee79aebfc3
jose luis <cortezluisjose@gmail.com>
2020-12-06 04:02:03 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,9 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	   
+	    @post = Post.new(post_params(:title, :description))
+  @post.save
+  redirect_to post_path(@post)
 	end
 
 	def update
